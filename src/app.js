1e9d5f0912612c6d7fcfa45ccf81723b74a50660
J0nathan-CR <jacr-work@outlook.com>
2025-07-15 03:47:38 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function getRandomElement(arr) {
+    if (!arr || arr.length=== 0) {
+      return undefined;
+    }
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  const whoWas = getRandomElement(who);
+  const actionWas = getRandomElement(action);
+  const whatWas = getRandomElement(what);
+  const whenWas = getRandomElement(when);
+
+  const randomExcuse = whoWas + " " + actionWas + " " + whatWas + " " + whenWas;
+
+  
+
+  document.getElementById("excuse").innerHTML = randomExcuse
+  console.log(randomExcuse);
 };
